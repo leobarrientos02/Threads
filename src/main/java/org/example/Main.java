3849183ea10c4f33c076aebbe538479d1e9d166f
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-06 15:16:18 -0400

    complete demo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,22 @@
 package org.example;
 
-class MyTask{
-    void executeTask(){
+//class MyTask{
+//    void executeTask(){
+//        for(int doc = 1; doc  <= 10; doc++){
+//            System.out.println("@@ Printing document #" + doc + "from Printer 2");
+//        }
+//    }
+//}
+
+// MyTask IS-A Thread
+class MyTask extends Thread{
+    @Override
+    public void run(){
         for(int doc = 1; doc  <= 10; doc++){
             System.out.println("@@ Printing document #" + doc + "from Printer 2");
         }
     }
+
 }
 
 public class Main {
@@ -19,17 +30,20 @@ public class Main {
         System.out.println("==Application Started==");
 
         // JOB2
-        MyTask task = new MyTask();
-        task.executeTask();
+        MyTask task = new MyTask(); // Child Thread or Worker Thread
+        //task.executeTask(); This would be the regular version
+        task.start(); // Start should internally execute the run method
 
         // Till Job2 is not finished, below written jobs are waiting and are not executed.
         // If Job2 is a long-running operation such as several document suppose to be printed
         // In such a use case OS/JVM shall give a message that the app is not responding
         // Such sluggish behavior in app can be observed -> App seems to hang.
 
+        // With Threads, main and MyTask are executing both parallelled or concurrently.
+
         // JOB3
         for(int doc = 1; doc  <= 10; doc++){
-            System.out.println("@@ Printing document #" + doc + "from Printer 1");
+            System.out.println("^^ Printing document #" + doc + "from Printer 1");
         }
 
         // JOB3
