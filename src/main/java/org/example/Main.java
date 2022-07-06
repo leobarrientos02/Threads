46f8f4d1bb3d56c01a3b2b6a8518cd62398450e6
leobarrientos02 <leobarrientos02@gmail.com>
2022-07-06 13:08:38 -0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,38 @@
+package org.example;
+
+class MyTask{
+    void executeTask(){
+        for(int doc = 1; doc  <= 10; doc++){
+            System.out.println("@@ Printing document #" + doc + "from Printer 2");
+        }
+    }
+}
+
+public class Main {
+    // main method is the main thread
+    public static void main(String[] args) {
+        // Whatever we write in here will be executed by main thread
+        // threads always execute the jobs in a sequence
+        // Execution Context: in a sequence
+
+        // JOB1
+        System.out.println("==Application Started==");
+
+        // JOB2
+        MyTask task = new MyTask();
+        task.executeTask();
+
+        // Till Job2 is not finished, below written jobs are waiting and are not executed.
+        // If Job2 is a long-running operation such as several document suppose to be printed
+        // In such a use case OS/JVM shall give a message that the app is not responding
+        // Such sluggish behavior in app can be observed -> App seems to hang.
+
+        // JOB3
+        for(int doc = 1; doc  <= 10; doc++){
+            System.out.println("@@ Printing document #" + doc + "from Printer 1");
+        }
+
+        // JOB3
+        System.out.println("==Application Finished==");
+    }
+}
